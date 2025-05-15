6faf27652eceecb63b25a5470461bd26e46d56e9
Ismail Alshaer <ismail.alshaer@gmail.com>
2025-05-15 20:46:34 +0200

    The 1st Rust version

---
+++ myapp01/target/debug/deps/myapp01.d
@@ -0,0 +1,5 @@
+E:\dev\rust\tuturials\myapp01\target\debug\deps\myapp01.exe: src\main.rs
+
+E:\dev\rust\tuturials\myapp01\target\debug\deps\myapp01.d: src\main.rs
+
+src\main.rs:
---
+++ myapp01/target/debug/myapp01.d
@@ -0,0 +1,1 @@
+E:\dev\rust\tuturials\myapp01\target\debug\myapp01.exe: E:\dev\rust\tuturials\myapp01\src\main.rs
