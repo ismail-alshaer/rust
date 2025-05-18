f67672547b54745c52e9c9e28020a9a16785b53a
Ismail Alshaer <ismail.alshaer@gmail.com>
2025-05-18 16:04:26 +0200

    update myapp01 and the new app myapp02

---
+++ myapp02/target/debug/deps/myapp02.d
@@ -0,0 +1,5 @@
+E:\dev\rust\tuturials\myapp02\target\debug\deps\myapp02.exe: src\main.rs
+
+E:\dev\rust\tuturials\myapp02\target\debug\deps\myapp02.d: src\main.rs
+
+src\main.rs:
---
+++ myapp02/target/debug/myapp02.d
@@ -0,0 +1,1 @@
+E:\dev\rust\tuturials\myapp02\target\debug\myapp02.exe: E:\dev\rust\tuturials\myapp02\src\main.rs
